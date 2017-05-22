8cca68e9654c4c7b2590e841a2f0a511ac59ff88
Mark Eschbach <mark@rhumbix.com>
2017-05-22 14:21:55 -0700

    Simplifying packaging information

--- setup.py
+++ setup.py
@@ -4,17 +4,9 @@ setup(name='syslogssl',
       version='0.1',
       description='RFC 5424 TLS',
       long_description='Provides Syslog over TLS conformant to the RFC 5424 descriptions of the protocol',
-      classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2.7'
-      ],
       keywords='syslog',
       url='https://github.com/lhl/python-syslogssl',
       author='lhl',
-      author_email='',
       license='MIT',
-      packages=['syslogssl'],
-      install_requires=[ ],
-      include_package_data=True,
-      zip_safe=False)
+      install_requires=[ ]
+      )
